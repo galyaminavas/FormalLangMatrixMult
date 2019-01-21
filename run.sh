d2033bbdca83e3b5585c498d55401ea871ef185f
Vasilisa <st047466@student.spbu.ru>
2019-01-21 14:08:01 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ INPUT_CHOMSKY_FILE=$1
 INPUT_GRAPH_FILE=$2
 OUTPUT_FILE=$3
 
+# firstly do: 
+# source ./FormalLangMatrixMult/new_venv/bin/activate
 python3 ./FormalLangMatrixMult/main.py $INPUT_CHOMSKY_FILE $INPUT_GRAPH_FILE $OUTPUT_FILE
